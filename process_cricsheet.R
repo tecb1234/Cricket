f3738a6f49f95f637c948c7068a134d75e9a690b
tecb1234 <t_baynes@hotmail.com>
2015-12-21 13:46:14 +0000

    clear out rest of old stuff and add helper functions for processing ball-by-ball data

--- process_cricsheet.R
+++ process_cricsheet.R
@@ -9,7 +9,7 @@ data <- yaml.load_file("C:/Users/Thomas/Documents/Analysis/Cricket/Cricsheet dat
 
 
 
-#deal with match info data
+#### deal with match info data
 files <- list.files("Cricsheet data")
 files <- files[1:(length(files)-1)]
 
@@ -26,42 +26,54 @@ info_dfs <- lapply(yaml_files, extract_info_df)
 
 combined_info_df <- rbind_all(info_dfs)
 
+#### deal with the ball-by-ball data from each match
 
-# convert the first innings data into a list of dataframes, one for each ball
-first_innings <- lapply(data$innings[[1]]$`1st innings`$deliveries,as.data.frame)
+##first some helper functions
 
-first_innings1 <- lapply(data$innings[[1]],as.data.frame)
-a <- first_innings1[[1]]
+clean_col_names <- function(split_col_name){
+  # this function does the final stage of cleaning up the column headings, by joining together the consituent parts
+  #   after we have split up the original parts
+  return <- paste(split_col_name[-(1:2)], collapse="-")
+}
 
-clean_up_ball <- function(ball_row_df){
+process_delivery <- function(delivery_list){
+  #this function takes the list of deliveries and converts it into a dataframe of just 1 row,
+  # with consistent column headings and values for the ball and over.
+  
+  
+  #convert to dataframe
+  delivery_df <- as.data.frame(delivery_list)
+  
   # get the current column headings
-  col_names <- colnames(ball_row_df)
+  col_names <- colnames(delivery_df)
   
   # remove the first x
   col_names <- str_sub(col_names,2,str_length(col_names))
   
   # split the column headings up so that we can access the over and ball data
-  split_col_name <- str_split(col_names, pattern='\\.')
+  split_col_name <- str_split(col_names, pattern='\\.')  
   
-  new_column_names <- lapply(split_col_name, clean_column_names)
-  
-  colnames(ball_row_df) <- new_column_names
-  
-  # extract the over and ball information from the old split up column names
   over <- data.frame(over = split_col_name[[1]][1])
   ball <- data.frame(ball = split_col_name[[1]][2])
   
-  # merge in the over and ball data into the data row
-  ball_row_df <- bind_cols(as.data.frame(over),as.data.frame(ball), ball_row_df)
-  return(ball_row_df)
+  #strip off the over and ball, i.e everything before the second dot, and update the df column headings
+  col_names <- lapply(split_col_name, clean_col_names)
+  
+  colnames(delivery_df) <- col_names
+  
+  #merge the over and ball information back in
+  delivery_df <- bind_cols(as.data.frame(over),as.data.frame(ball), delivery_df)
+  return <- delivery_df
 }
 
-first_innings<- lapply(first_innings, clean_up_ball)
+process_innings <- function(innings_list){
+  # this function applies to the process_delviery function to each delivery list in an innings
+  # and joins all of the single row dataframes into one dataframe for the whole innings
+  innings_df <- lapply(innings_list[[1]]$deliveries, process_delivery)
+  innings_df <- rbind_all(innings_df)
+  return <- innings_df
+}
 
-
-first_innings_df <- first_innings %>%
-  bind_rows() %>%
-  as.data.frame()
 
 
 
