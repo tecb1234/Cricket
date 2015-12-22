64246d9ba1a047a75c3d8a5025d3da86cbefdca6
tecb1234 <t_baynes@hotmail.com>
2015-12-22 16:03:42 +0000

    add overaching test for my string manipulations and specific one for the filename extractor

--- test_cricket.R
+++ test_cricket.R
@@ -7,3 +7,10 @@ test_that("City to Country mapping", {
   expect_equal(as.character(geocode("Sydney", output = "more")$country), "Australia")
   
 })
+
+test_that("string manipulations", {
+  
+  expect_equal("file", get_filename("folder/file.yaml"))
+})
+
+
