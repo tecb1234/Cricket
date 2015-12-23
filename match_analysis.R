a15b57c0784683355a49b4342b7fb45984c6ebf3
tecb1234 <t_baynes@hotmail.com>
2015-12-23 15:21:20 +0000

    adding in various plots, some of which don't work properly yet.

--- match_analysis.R
+++ match_analysis.R
@@ -1,5 +1,7 @@
 #this script will do some analysis at the individual match level
 
+library(ggplot2)
+
 source("process_cricsheet.R")
 
 #get the win/loss record for each team
@@ -13,4 +15,29 @@ team_summary <- matches_df %>%
 
 
 
+p <- ggplot(team_summary, aes( x = teams, y = wins)) + geom_bar(stat = "identity") +
+        theme(axis.title.x = element_blank(),
+              axis.text.x  = element_text(angle=90, vjust=0.3, hjust = 1, size=10))
+
+p
+
+#reorder the levels in teams so that the bars are in the plot are in the right order
+
+team_summary$teams <- factor(team_summary$teams, levels = team_summary$teams[order(team_summary$wins, decreasing = TRUE)])
+
+order(team_summary$wins)
+
+
+q <- ggplot(team_summary, aes( x = teams, y = wins)) + geom_bar(stat = "identity") +
+  theme(axis.title.x = element_blank(),
+        axis.text.x  = element_text(angle=90, vjust=0.3, hjust = 1, size=10))
+
+q
+
+
+ggplot(team_summary) + 
+  geom_bar(aes(x = teams, y = wins), stat = "identity", fill = "Green") +
+  geom_bar(aes(x = teams, y = losses), stat = "identity", fill = "Red") +
+  coord_flip() +
+  facet_wrap(~teams)
  
