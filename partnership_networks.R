53c14833725d171e9a1595cb31d784551a52b0b2
tecb1234 <t_baynes@hotmail.com>
2015-12-31 15:42:22 +0000

    code to build network diagrams  of batting partnerships

--- partnership_networks.R
+++ partnership_networks.R
@@ -1,5 +1,7 @@
 ## Partnerships
 
+library(networkD3)
+
 batsman_at_crease <- ball_by_ball_df %>%
   gather(batting_role, batter, -c(1:4, 6, 8:length(ball_by_ball_df))) %>%
   arrange(match_id, batting_side, over, ball)
@@ -13,55 +15,30 @@ partnerships_by_match <- ball_by_ball_df %>%
             runs_batsman = sum(`runs-batsman`),
             runs_extras = sum(`runs-extras`))
 
-partnerships_summary <- ball_by_ball_df %>%
-  group_by(batting_side, batsman, non_striker) %>%
-  summarise(balls_faced = n(),
-            runs_total = sum(`runs-total`),
-            runs_batsman = sum(`runs-batsman`),
-            runs_extras = sum(`runs-extras`))
+countries <- batsman_at_crease %>%
+  distinct(batting_side) %>%
+  select(batting_side)
 
-library(networkD3)
-
-england_partners <- partnerships_summary %>%
-    filter(batting_side == "England")
-
-
-simpleNetwork(england_partners, Source = "batsman", Target = "non_striker")
-
-england_partners_reduced <- england_partners %>%
-    filter(balls_faced > 6)
-
-simpleNetwork(england_partners_reduced, Source = "batsman", Target = "non_striker")
-
-england_players <- batsman_at_crease %>%
-    filter(batting_side == "England") %>%
-    select(batter) %>%
-    distinct(batter) %>%
-    mutate(node_key = 0:(n()-1)) %>%
-    mutate(Group = rep(1,n()))
-
-england_partners_links <- england_partners %>%
-    left_join(england_players, by = c("batsman" = "batter")) %>%
-    rename(batter_node_key = node_key) %>%
-    left_join(england_players, by = c("non_striker" = "batter")) %>%
-    rename(non_striker_node_key = node_key) 
-                
-forceNetwork(Links = england_partners_links, 
-             Nodes = england_players,
-             NodeID = "batter",
-             Source = "batter_node_key", 
-             Target = "non_striker_node_key", Group = "Group", Value = "runs_total")
-
-
-england_partners_links_reduced <- england_partners %>%
-  filter(runs_total > 20) %>%
-  left_join(england_players, by = c("batsman" = "batter")) %>%
-  rename(batter_node_key = node_key) %>%
-  left_join(england_players, by = c("non_striker" = "batter")) %>%
-  rename(non_striker_node_key = node_key) 
-
-forceNetwork(Links = england_partners_links_reduced, 
-             Nodes = england_players,
-             NodeID = "batter",
-             Source = "batter_node_key", 
-             Target = "non_striker_node_key", Group = "Group", Value = "runs_total")
+# england_nodes <- batsman_at_crease %>%
+#   select(batting_side, batter, match_id) %>%
+#   filter(batting_side == "England") %>%
+#   filter(match_id == 211028) %>%
+#   distinct(batter) %>%
+#   mutate(node_key = 0:(n()-1)) %>%
+#   mutate(Group = rep(1,n()))
+#   
+# england_links <- partnerships_by_match %>%
+#   filter(batting_side == "England") %>%
+#   filter(match_id == 211028) %>%
+#     left_join(england_nodes, by = c("batsman" = "batter", "batting_side", "match_id")) %>%
+#   rename(batter_node_key = node_key) %>%
+#   left_join(england_nodes, by = c("non_striker" = "batter", "batting_side", "match_id")) %>%
+#   rename(non_striker_node_key = node_key)
+# 
+# forceNetwork(Links = england_links,
+#              Nodes = england_nodes,
+#              NodeID = "batter",
+#              Source = "batter_node_key", 
+#              Target = "non_striker_node_key", 
+#              Group = "Group", 
+#              Value = "runs_total")
