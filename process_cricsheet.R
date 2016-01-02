e451908078c4ded79a0afe72018d1fdad2c3bda5
tecb1234 <t_baynes@hotmail.com>
2016-01-02 13:01:52 +0000

    add column to the ball_by_ball dataframe that shows whether the team batting is batting first or chasing.

--- process_cricsheet.R
+++ process_cricsheet.R
@@ -165,6 +165,11 @@ process_match <- function(yaml_file) {
   #This function applies the process_innings function to each innings in the match 
   # to create a single dataframe and adds a match_id col
   both_innings_dfs <- lapply(yaml_file$innings[1:2], process_innings)
+  
+  both_innings_dfs[[1]] <- both_innings_dfs[[1]] %>%
+    mutate(bat_first_chasing = "bat_first")
+  both_innings_dfs[[2]] <- both_innings_dfs[[2]] %>%
+    mutate(bat_first_chasing = "chasing")
   both_innings_df <- rbind_all(both_innings_dfs)
   
   match_id <- yaml_file$match_id
