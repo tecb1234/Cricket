879fe8297841f4cbf36064c418d8acfb0297791d
tecb1234 <t_baynes@hotmail.com>
2016-03-29 18:32:48 +0100

    Rename partnership_networks because it doesn't really do the network bit.
    Put back the batsman_at_crease bit as it's needed for the nodes

--- partnerships.R
+++ partnerships.R
@@ -3,6 +3,10 @@
 library(dplyr)
 library(tidyr)
 
+batsman_at_crease <- ball_by_ball_df %>%
+  gather(batting_role, batter, -c(1:4, 6, 8:length(ball_by_ball_df))) %>%
+  arrange(match_id, batting_side, over, ball)
+
 partnerships_by_match <- ball_by_ball_df %>%
   group_by(match_id, batting_side, bowling_side, batsman, non_striker) %>%
   summarise(wicket = first(wicket),
--- shiny/partnerships/partnerships_shiny_helper.R
+++ shiny/partnerships/partnerships_shiny_helper.R
@@ -5,7 +5,7 @@ project_root <- find_root(criterion = is_rstudio_project)
 
 load(file.path(project_root,"matches.RData"))
 load(file.path(project_root,"ball_by_ball.RData"))
-source(file.path(project_root,"partnership_networks.R"))
+source(file.path(project_root,"partnerships.R"))
 
 countries <- ball_by_ball_df %>%
   distinct(batting_side) %>%
